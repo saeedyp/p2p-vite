2317e9d323b7f5bda1d83b7875b60bd0a17cb632
saeedyahyapour <sd0yp@yahoo.com>
2023-11-30 22:05:09 +0330

    pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,55 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      // registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'android-chrome-192x192.png',
+        'android-chrome-512x512.png'
+      ],
+      manifest: {
+        name: 'P2P Web App',
+        short_name: 'P2pVite',
+        description: 'Installable app to p2p call',
+        theme_color: '#333c4d',
+        background_color: '#0f172a',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     port: 8080,
     strictPort: true,
@@ -15,9 +60,6 @@ export default defineConfig({
   build: {
     // assetsDir: 'assets',
   },
-  plugins: [
-    vue(),
-  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
